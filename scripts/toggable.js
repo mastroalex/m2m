2317a5521601336e4638c7d38bb111d3d82978d6
Alessandro Mastrofini <alessandrom@wolfram.com>
2024-09-01 20:54:10 +0200

    Fix

--- scripts/toggable.js
+++ scripts/toggable.js
@@ -1,4 +1,4 @@
-ocument.addEventListener('DOMContentLoaded', () => {
+Document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll('.toggable-section');
 
     sections.forEach(section => {
