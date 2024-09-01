e79da7a21836dd6ea3b0d53d5d4483db9188b379
Alessandro Mastrofini <alessandrom@wolfram.com>
2024-09-01 20:48:56 +0200

    Toggable test

---
+++ scripts/toggable.js
@@ -0,0 +1,14 @@
+ocument.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.section');
+
+    sections.forEach(section => {
+        const header = section.querySelector('.header');
+        const content = section.querySelector('.section-content');
+        const icon = section.querySelector('.toggle-icon');
+
+        header.addEventListener('click', () => {
+            content.classList.toggle('open');
+            icon.classList.toggle('open');
+        });
+    });
+});
